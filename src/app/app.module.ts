0f78071c0f06c24cdbd336a0b447ac6906cde0fe
dalejac <k-da929@hotmail.com>
2020-11-12 11:03:42 -0500

    agregar componentes a los module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,22 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 import { HttpClientModule } from '@angular/common/http';
-import { MenuComponent } from './waiterModule/menu/menu.component';
+import { WaiterModule } from '../app/waiter/waiter.module';
 import { HomeComponent } from './home/home.component';
 import { ProductsService } from './products.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    HttpClientModule
+    HttpClientModule,
+    WaiterModule
   ],
   providers:
   [
