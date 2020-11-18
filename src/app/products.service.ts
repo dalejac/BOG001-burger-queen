506187de10b4ef7403b58b5ecc541faff43cece0
dalejac <k-da929@hotmail.com>
2020-11-18 10:42:02 -0500

    Agregamos la interface de menu y modificamos los elementos

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Products } from 'src/app/model/product.model';
 import { HttpClient } from '@angular/common/http';
 import * as data from '../assets/menu.json';
+import { Menu } from './model/menu.model';
 
 
 
@@ -13,7 +13,7 @@ import * as data from '../assets/menu.json';
 
 export class ProductsService {
 
-  private products: Products[];
+  private products: Menu;
 
   constructor(private httpClient: HttpClient) {
 
@@ -21,8 +21,8 @@ export class ProductsService {
 
   apiURL = '../assets/menu.json';
 
-  getProducts(): Observable<Products[]> {
-    return this.httpClient.get<Products[]>(`${this.apiURL}`);
+  getProducts(): Observable<Menu> {
+    return this.httpClient.get<Menu>(`${this.apiURL}`);
   }
 }
 console.log(data);
