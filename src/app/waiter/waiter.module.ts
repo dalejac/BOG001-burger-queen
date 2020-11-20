4c1f72ed00940b154b815e79792ec976b37ec1a0
Nathiuk <nathaliasaray0220@gmail.com>
2020-11-20 13:13:23 -0500

    Agregamos el componente de lunch al modulo

--- src/app/waiter/waiter.module.ts
+++ src/app/waiter/waiter.module.ts
@@ -5,13 +5,14 @@ import { WaiterRoutingModule } from './waiter-routing.module';
 import { WaiterComponent } from './waiter/waiter.component';
 import { MenuComponent } from './menu/menu.component';
 import { BreakfastComponent } from './breakfast/breakfast.component';
-
+import { LunchComponent } from './lunch/lunch.component'; 
 
 @NgModule({
   declarations: [
     WaiterComponent,
     MenuComponent,
-    BreakfastComponent
+    BreakfastComponent,
+    LunchComponent
   ],
 
   imports: [
