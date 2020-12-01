5483d20c4900e3b60976bcf5cbbaa83b2e582107
dalejac <k-da929@hotmail.com>
2020-12-01 10:25:28 -0500

    NgModel for inputs and creating the ticket view of the order

--- src/app/waiter/waiter.module.ts
+++ src/app/waiter/waiter.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { WaiterRoutingModule } from './waiter-routing.module';
 import { WaiterComponent } from './waiter/waiter.component';
 import { MenuComponent } from './menu/menu.component';
 import { BreakfastComponent } from './breakfast/breakfast.component';
-import { LunchComponent } from './lunch/lunch.component'; 
+import { LunchComponent } from './lunch/lunch.component';
+import { TicketComponent } from './ticket/ticket.component'; 
 
 @NgModule({
   declarations: [
     WaiterComponent,
     MenuComponent,
     BreakfastComponent,
-    LunchComponent
+    LunchComponent,
+    TicketComponent
   ],
 
   imports: [
     CommonModule,
+    FormsModule,
     WaiterRoutingModule
   ]
 })
