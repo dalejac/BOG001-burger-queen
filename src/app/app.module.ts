a20e59505d05c8491f51ba9d38f3016d706368aa
dalejac <k-da929@hotmail.com>
2020-12-01 10:23:06 -0500

    Forms Module import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
@@ -21,6 +22,7 @@ import { ProductsService } from './products.service';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     HttpClientModule,
+    FormsModule,
     WaiterModule
   ],
   providers:
